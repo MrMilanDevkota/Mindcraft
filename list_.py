734c0259a1a613589e74340d16ffd400c507498b
Milan <devkotamilan852@gmail.com>
2025-09-14 13:47:49 +0545

    list ideas

---
+++ list_.py
@@ -0,0 +1,6 @@
+list1=['milan', 'swostika', 'dikshya','nabina']
+list2= [1,2,3,4]
+print(list1[0])
+
+list1[0]=1
+print(list1[0])
---
+++ list_methods_.py
@@ -0,0 +1,26 @@
+# working on list and its method 
+list1= [2,1,7,9,5,0,1]
+
+print(len(list1))
+
+# sorted_list= list1.sort()
+# print(list1)
+
+# list1.append(6)
+# print(list1)
+
+# list1.sort(reverse=True)
+# print(list1)
+
+# list1.insert(2,3)
+# print(list1)
+
+# list1.remove(1)
+# print(list1)
+
+# list1.pop(2)
+# print(list1)
+
+# list1.extend([2,3,4])
+# print(list1)
+
